045ddec379ca67fb1574c498dfef563ef81eee0e
jichani <jichan7571@naver.com>
2023-05-23 15:46:25 +0900

    토글 구현 완료

--- main.js
+++ main.js
@@ -7,3 +7,22 @@ window.addEventListener('scroll', function () {
         header.classList.remove('slider');
     }
 });
+
+// 토글 메뉴 클릭했을 때
+const toggleBtn = document.querySelector(".navbar__toggleBtn");
+const menu = document.querySelector(".navbar__menu");
+const icons = document.querySelector(".navbar__icons");
+
+toggleBtn.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    icons.classList.toggle("active");
+    toggleBtn.classList.toggle("active");
+});
+
+// 검색 메뉴 클릭했을 때
+const searchBtn = document.querySelector(".navbar__search__Btn")
+const searchContainer = document.querySelector(".search");
+
+searchBtn.addEventListener("click", () => {
+    searchContainer.classList.toggle("active")
+})
